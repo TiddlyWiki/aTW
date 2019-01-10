39fe0037b6e35d0418b916b6e44058e55d36a1f5
BurningTreeC <hypnotizedkangaroo@gmail.com>
2019-01-10 10:14:07 +0100

    fix 'getText' to 'getTiddlerText' + set system ui colors

--- mobile/src/main/java/com/tiddlywiki/atw/AtwWebViewClient.java
+++ mobile/src/main/java/com/tiddlywiki/atw/AtwWebViewClient.java
@@ -2,20 +2,27 @@ package com.tiddlywiki.atw;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
+import android.util.Log;
+import android.view.Window;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import static android.content.ContentValues.TAG;
 
 public class AtwWebViewClient extends WebViewClient {
 
     private Context mContext;
     private WebView mWebView;
+    private Window mWindow;
 
     /** Instantiate the interface and set the context */
-    AtwWebViewClient (Context c, WebView v) {
+    AtwWebViewClient (Context c, WebView v,Window w) {
         mContext = c;
         mWebView = v;
+        mWindow = w;
     }
 
     @Override
@@ -36,18 +43,32 @@ public class AtwWebViewClient extends WebViewClient {
     @Override
     public void onPageFinished(final WebView view, String url) {
         //extract favicon and siteTitle from wiki, for use in landing page
-        mWebView.evaluateJavascript("javascript:$tw.wiki.getText('favicon.ico');",
+        mWebView.evaluateJavascript("javascript:$tw.wiki.getTiddlerText('favicon.ico');",
                 new ValueCallback<String>() {
                     @Override
                     public void onReceiveValue(String s) {
                         saveLandingPageAsset(mWebView.getUrl(),"favicon",s);
                     }
                 });
-        mWebView.evaluateJavascript("javascript:$tw.wiki.getText('$:/SiteTitle');",
+        mWebView.evaluateJavascript("javascript:$tw.wiki.getTiddlerText('$:/SiteTitle');",
                 new ValueCallback<String>() {
                     @Override
                     public void onReceiveValue(String s) {
                         saveLandingPageAsset(mWebView.getUrl(),"sitetitle",s);
+                    }
+                });
+        //set system system ui colors to wiki background
+        //TODO: when background changes, set colors again
+        mWebView.evaluateJavascript("javascript:$tw.wiki.extractTiddlerDataItem($tw.wiki.getTiddlerText('$:/palette'),'page-background');",
+                new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String colorString) {
+                        try {
+                            mWindow.setStatusBarColor(Color.parseColor(colorString.replaceAll("\"","")));
+                            mWindow.setNavigationBarColor(Color.parseColor(colorString.replaceAll("\"","")));
+                        } catch (Exception e) {
+                            Log.e(TAG, e.getMessage());
+                        }
                     }
                 });
     }
